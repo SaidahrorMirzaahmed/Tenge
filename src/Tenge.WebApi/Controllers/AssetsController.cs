dc47a0bf825901820bb2d35880fa6c9b0e2f6b8f
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2024-08-13 12:08:42 +0500

    Added AssetsController

---
+++ src/Tenge.WebApi/Controllers/AssetsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.Marshalling;
+using Tenge.Service.Assets.Service;
+using Tenge.WebApi.Models.Assets;
+using Tenge.WebApi.Models.Responses;
+
+namespace Tenge.WebApi.Controllers;
+
+[EnableCors("AllowSpecificOrigin")]
+[Route("api/[controller]")]
+[ApiController]
+public class AssetsController(IAssetService service) : ControllerBase
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> UploadPictureOnly([FromBody]AssetCreateModel model)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "OK",
+            Data = await service.UploadAsync(model)
+        });
+    }
+}
